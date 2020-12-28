06a782864cec5af83595f02b15edb964c39893ed
Kelvin Ronaldo <kelvin.ronaldo@hotmail.com>
2020-12-28 16:15:13 -0300

    35 - (*) Zero ou Mais

---
+++ quantificadores/ZeroOuMais.js
@@ -0,0 +1,7 @@
+const texto = 'De longe eu avistei o fogo e uma pessoa gritando: FOGOOOOO!!';
+const texto2 = 'There is s big fog in NYC';
+
+// * = Zero ou mais ocorrências
+const regex = /fogo*/gi;
+console.log(texto.match(regex));
+console.log(texto2.match(regex));
